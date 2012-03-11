9d063156872083ebd285b0f1c863bea0fea8019f
Lance Hunt <lancehunt@gmail.com>
2012-03-11 16:27:27 -0500

    Added Get-Loc, Set-Loc, and Go-Loc helpers to home.psm1

--- Modules/Helpers/home.psm1
+++ Modules/Helpers/home.psm1
@@ -22,6 +22,33 @@ function Set-Home {
 	[System.Environment]::SetEnvironmentVariable("Home-Folder", $homeLocation, "User")
 }
 
+# .SYNOPSIS 
+# Get the current home folder
+# .LINK
+# Set-Home
+# Go-Home
+function Get-Loc($locNum) {  return [System.Environment]::GetEnvironmentVariable("Home-Location$locNum", "User") }
+
+# .SYNOPSIS 
+# Go to the home folder 
+# .LINK
+# Get-Home
+# Set-Home
+function Go-Loc($locNum) { 
+  $location = Get-Loc($locNum)
+  Set-Location $location
+}
+
+# .SYNOPSIS 
+# Set the current location as the home folder
+# .LINK
+# Get-Home
+# Go-Home
+function Set-Loc($locNum) { 
+	$location = Get-Location
+	[System.Environment]::SetEnvironmentVariable("Home-Location$locNum", $location, "User")
+}
+
 $homeLocation = Get-Home
 
 Export-ModuleMember -Function * -Alias *
